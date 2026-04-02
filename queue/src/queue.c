fd5820a50aea44619dad2707b7308b2386250eff
WhiteWolf313 <slaiver@live.se>
2026-04-02 18:21:04 +0000

    level-7

---
+++ queue/src/queue.c
@@ -0,0 +1,97 @@
+#include "pico/stdlib.h"
+#include "pico/util/queue.h"
+#include <stdio.h>
+
+#define BTN_A_PIN 14
+#define BTN_B_PIN 15
+#define BTN_RESET_PIN 16
+
+#define DELAY_MS 250
+#define STARTUP_DELAY_MS 2000
+
+#define NUMER_OF_Q_ELEMENTS 16
+
+volatile uint32_t counter = 0;
+// Queue for events
+queue_t q;
+
+typedef struct {
+    uint gpio;
+    uint32_t events;
+} event_t;
+
+void slow_process(uint gpio) {
+    printf("Start %d from GPIO %d\n", counter++, gpio);
+
+    printf("Step A\n"); sleep_ms(DELAY_MS);
+    printf("Step B\n"); sleep_ms(DELAY_MS);
+    printf("Step C\n"); sleep_ms(DELAY_MS);
+    printf("Step D\n"); sleep_ms(DELAY_MS);
+
+    printf("Done\n");
+}
+
+void gpio_isr(uint gpio, uint32_t events) {
+    if (gpio == BTN_RESET_PIN) {
+        printf("Resetting counter\n");
+        counter = 0;
+        printf("Counter reset\n");
+        return;
+    }
+    printf("GPIO %d pressed\n", gpio);
+    event_t e = {
+        .gpio = gpio,
+        .events = events
+    };
+    queue_try_add(&q, &e);
+}
+
+// --------------------
+// Init helpers
+// --------------------
+void init_btn(int pin) {
+    printf("Initializing GPIO %d\n", pin);
+    gpio_init(pin);
+    gpio_set_dir(pin, GPIO_IN);
+    gpio_pull_up(pin);
+}
+
+void init_all() {
+    printf("Initializing system\n");
+
+    queue_init(&q, sizeof(event_t), NUMER_OF_Q_ELEMENTS);
+
+    init_btn(BTN_A_PIN);
+    init_btn(BTN_B_PIN);
+    init_btn(BTN_RESET_PIN);
+
+    gpio_set_irq_enabled_with_callback(BTN_A_PIN,GPIO_IRQ_EDGE_FALL,true,&gpio_isr);
+    gpio_set_irq_enabled_with_callback(BTN_B_PIN,GPIO_IRQ_EDGE_FALL,true,&gpio_isr);
+    gpio_set_irq_enabled_with_callback(BTN_RESET_PIN,GPIO_IRQ_EDGE_FALL,true,&gpio_isr);
+}
+
+int main() {
+    stdio_init_all();
+    sleep_ms(STARTUP_DELAY_MS);
+
+    init_all();
+
+    while (1) {
+        event_t e;
+        if (queue_try_remove(&q, &e)) {
+            switch (e.gpio) {
+            case BTN_A_PIN:
+                printf("GPIO %d handled\n", e.gpio);
+                slow_process(e.gpio);
+                break;
+            case BTN_B_PIN:
+                printf("GPIO %d handled\n", e.gpio);
+                slow_process(e.gpio);
+                break;
+            default:
+                break;
+            }
+        }
+        tight_loop_contents();
+    }
+}
